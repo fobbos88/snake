9884856027dc2c0526ce1fde67de9ed7fec51479
fobbos88 <ctpaxt300@bk.ru>
2017-03-19 00:40:49 +0300

    LIST

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -15,7 +15,27 @@ namespace snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+                    List<char> numList = new List<char>();
+                    numList.Add('%');
+                    numList.Add('^');
+                    numList.Add('#');
+                    Point p3 = new Point(2, 6, '&');
+                    Point p4 = new Point(5, 7, '$');
             
+
+                    foreach (char i in numList)
+                    {
+                        Console.Write(i);
+                    }
+
+                    List<Point> pList = new List<Point>();
+                    pList.Add(p1);
+                    pList.Add(p2);
+                    pList.Add(p3);
+                    pList.Add(p4);
+
+                    
 
             Console.ReadLine();
         }
