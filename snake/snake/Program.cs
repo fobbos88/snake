eed7782485106f27e6d03a5781b62f1044317264
fobbos88 <ctpaxt300@bk.ru>
2017-03-19 21:04:41 +0300

    LinesXY

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,32 +11,19 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw(); 
+            p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-                    List<char> numList = new List<char>();
-                    numList.Add('%');
-                    numList.Add('^');
-                    numList.Add('#');
-                    Point p3 = new Point(2, 6, '&');
-                    Point p4 = new Point(5, 7, '$');
+            HorisontalLine line = new HorisontalLine(5, 10, 8, '+' );
+
+            VerticalLine Line_V = new VerticalLine(5, 15, 1, '%');
+
+            line.Draw();
+            Line_V.Draw();
             
-
-                    foreach (char i in numList)
-                    {
-                        Console.Write(i);
-                    }
-
-                    List<Point> pList = new List<Point>();
-                    pList.Add(p1);
-                    pList.Add(p2);
-                    pList.Add(p3);
-                    pList.Add(p4);
-
-                    
-
+         
             Console.ReadLine();
         }
         
